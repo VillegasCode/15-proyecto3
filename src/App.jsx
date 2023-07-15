b56a756f85ff7045c4c9db29dc0ef41ff23ed06c
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-15 13:19:04 -0500

    Import RUTAS like component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import { Articulos } from './components/pages/Articulos'
 import { Inicio } from './components/pages/Inicio'
 import { Crear } from './components/pages/Crear'
+import { Rutas } from './routing/Rutas'
 
 function App() {
   
   return (
-    <div>
+    <div className='layout'>
       <h1>Blog con React</h1>
-      <Inicio />
-      <Articulos />
-      <Crear />
+      <Rutas/>
     </div>
   )
 }
