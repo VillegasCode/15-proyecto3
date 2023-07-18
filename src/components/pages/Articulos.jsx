e7ff4cc0e84ecbad54c472ca4185d737814ce2bc
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-18 17:57:11 -0500

    If object namwed articulos.length is major to 1 then execute articulos.map else show a message with text Articulos no disponibles

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { Global } from '../../helpers/Global';
 
 export const Articulos = () => {
 
@@ -31,51 +32,59 @@ export const Articulos = () => {
 
 
   useEffect(() => {
-    
+
     conseguirArticulos();
 
   }, [])
 
-  const conseguirArticulos = async() => {
-    const url = "http://localhost:3900/api/articulos";
+  const conseguirArticulos = async () => {
+    const url = Global.url + "articulos";
     let peticion = await fetch(url, {
       method: "GET"
     });
     let datos = await peticion.json();
 
     //Para saber que props tiene el objeto datos
-    console.log (datos);
+    console.log(datos);
 
     // Si la prop status del objeto datos es "success" entonces el useState setArticulos se cargará con .articulos del objeto datos
-    if (datos.status == "success"){
+    if (datos.status == "success") {
       setArticulos(datos.articulos);
     }
   }
 
 
-// El objeto articulos del hook ya cargado con el array lo recorremos con el método .map
+  // El objeto articulos del hook ya cargado con el array lo recorremos con el método .map
   return (
     <>
-  {/* Creamos un objeto llamado articulo que contiene los datos recorridos por el array */}
-    {articulos.map(articulo => {
-      return (
-      // Creamos una key para react pueda identificar donde poner la información con los datos del array
-        <article key={articulo._id} className="articulo-item">
-        <div className='mascara'>
-          <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png'></img>
-        </div>
-        <div className='datos'>
-          <h3 className="title">{articulo.titulo}</h3>
-          <p className="description">{articulo.contenido}</p>
+      {
+        articulos.length >= 1 ? (
+          // {/* Creamos un objeto llamado articulo que contiene los datos recorridos por el array */ }
+          articulos.map(articulo => {
+            return (
+              // Creamos una key para react pueda identificar donde poner la información con los datos del array
+              <article key={articulo._id} className="articulo-item">
+                <div className='mascara'>
+                  <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png'></img>
+                </div>
+                <div className='datos'>
+                  <h3 className="title">{articulo.titulo}</h3>
+                  <p className="description">{articulo.contenido}</p>
 
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
+                  <button className="edit">Editar</button>
+                  <button className="delete">Borrar</button>
 
-        </div>
-      </article>
-      );
-    })}
+                </div>
+              </article>
+            );
+          })
+        )
+          :
+          (
+            <h1>No hay artículos disponibles</h1>
+          )
+      }
     </>
   )
 
-  }
+}
