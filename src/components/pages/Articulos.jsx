7949232d722a0a828e574d346e73f7e41e259502
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-18 13:48:26 -0500

    Creating a function named conseguirArticulos to consume an API REST with HTTP methods with HOOKS

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,7 +1,75 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export const Articulos = () => {
+
+  // Creamos un hook useState con el objeto articulos y su configuración setArticulos
+  const [articulos, setArticulos] = useState([]);
+
+  // Creamos un hook useEffect que cargue al inicio con los corchetes y lo cargamos con un array de objetos llamado data y se lo pasamos al hook setArticulos
+  // useEffect(() => {
+  //   let data = [
+  //     {
+  //       _id: 1,
+  //       titulo: "Titulo 1",
+  //       contenido: "Contenido"
+  //     },
+  //     {
+  //       _id: 2,
+  //       titulo: "Titulo 2",
+  //       contenido: "Contenido 2"
+  //     },
+  //     {
+  //       _id: 3,
+  //       titulo: "Titulo 3",
+  //       contenido: "Contenido 3"
+  //     },
+  //   ];
+
+  //   setArticulos(data);
+  // }, [])
+
+
+  useEffect(() => {
+    
+    conseguirArticulos();
+
+  }, [])
+
+  const conseguirArticulos = async() => {
+    const url = "http://localhost:3900/api/articulos";
+    let peticion = await fetch(url, {
+      method: "GET"
+    });
+    let datos = await peticion.json();
+
+    console.log (datos);
+  }
+
+
+// El objeto articulos del hook ya cargado con el array lo recorremos con el método .map
   return (
-    <div>Articulos</div>
+    <>
+  {/* Creamos un objeto llamado articulo que contiene los datos recorridos por el array */}
+    {articulos.map(articulo => {
+      return (
+      // Creamos una key para react pueda identificar donde poner la información con los datos del array
+        <article key={articulo._id} className="articulo-item">
+        <div className='mascara'>
+          <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png'></img>
+        </div>
+        <div className='datos'>
+          <h3 className="title">{articulo.titulo}</h3>
+          <p className="description">{articulo.contenido}</p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+
+        </div>
+      </article>
+      );
+    })}
+    </>
   )
-}
+
+  }
