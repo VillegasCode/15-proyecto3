06d9ae39f50a3a95d9271cc724a2b631a4e7fabc
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-18 23:09:54 -0500

    Creation of the form with className form-group into Crear.jsx component

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export const Crear = () => {
   return (
-    <div>Crear</div>
+    <div className='jumbo'>
+      <h1>Crear Artículo</h1>
+      <p>Formulario para crear artículo</p>
+
+      {/* Montar formulario */}
+      <form className='formulario'>
+
+        <div className='form-group'>
+          <label htmlFor="titulo">TÍTULO</label>
+          <input type='text' name='titulo' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor="contenido">CONTENIDO</label>
+          <textarea type='text' name='contenido' />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor="file0">IMAGEN</label>
+          <input type='file' name='file0' id='file' />
+        </div>
+
+        <input type='submit' value="Guardar" className='btn btn-success' />
+
+      </form>
+
+
+    </div>
   )
 }
