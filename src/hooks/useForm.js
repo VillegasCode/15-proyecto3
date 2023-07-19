a082b4627f303b481a4182e8c119c050545b7fdc
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-18 19:51:05 -0500

    Copy of the 14-mas-hooks project

---
+++ src/hooks/useForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+export const useForm = ( objetoIncial = {} ) => {
+
+const [formulario, setFormulario] = useState(objetoIncial);
+
+  const serializarFormulario = (formulario) => {
+    const formData = new FormData(formulario);
+
+    const objetoCompleto = {};
+
+    for(let [name, value] of formData){
+      objetoCompleto[name] = value;
+    }
+
+    return objetoCompleto;
+  }
+
+  const enviado = (e) =>{
+      e.preventDefault();
+      let curso = serializarFormulario(e.target);
+
+      setFormulario(curso);
+
+      document.querySelector(".codigo").classList.add("enviado");
+  }
+
+  const cambiado = ({target}) => {
+    const {name, value} = target;
+
+    setFormulario({
+      ...formulario,
+      [name]: value
+    });
+  }
+
+  return {
+    formulario,
+    enviado,
+    cambiado
+  }
+}
