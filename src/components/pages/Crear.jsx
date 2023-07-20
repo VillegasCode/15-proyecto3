d5ab680e4f77ae7f4e0a583691ee2e093b803ae0
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-19 21:08:02 -0500

    Creating desestructure to useForm with 3 params formulario, enviado, cambiado and creation of guardarArticulo method

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {useState} from "react";
+import {useForm} from "../../hooks/useForm";
 
 export const Crear = () => {
+
+  const {formulario, enviado, cambiado} = useForm({});
+
+  const guardarArticulo = (e) => {
+    e.preventDefault();
+
+    //Recoger datos formulario
+    let nuevoArticulo = JSON.stringify(formulario);
+    console.log(nuevoArticulo)
+
+    //Guardar articulo en el backend
+    
+  }
+
   return (
     <div className='jumbo'>
       <h1>Crear Artículo</h1>
       <p>Formulario para crear artículo</p>
+      <pre>{JSON.stringify(formulario)}</pre>
 
       {/* Montar formulario */}
-      <form className='formulario'>
+      <form className='formulario' onSubmit={guardarArticulo}>
 
         <div className='form-group'>
           <label htmlFor="titulo">TÍTULO</label>
-          <input type='text' name='titulo' />
+          <input type='text' name='titulo' onChange={cambiado} />
         </div>
 
         <div className='form-group'>
           <label htmlFor="contenido">CONTENIDO</label>
-          <textarea type='text' name='contenido' />
+          <textarea type='text' name='contenido' onChange={cambiado} />
         </div>
 
         <div className='form-group'>
