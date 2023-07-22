ad09fd3b34ef7a8b917fe0d6f643f6b8f3fb080b
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-22 01:19:28 -0500

    Fix error with close key

--- src/helpers/Peticion.js
+++ src/helpers/Peticion.js
@@ -1,36 +1,43 @@
-export const Peticion = async(url, metodo, datosGuardar = "") => {
-    
+export const Peticion = async (url, metodo, datosGuardar = "", archivos = false) => {
+
     let cargando = true;
 
+    let opciones = {
+        method: "GET"
+    };
 
-
-        let opciones = {
-            method: "GET"
+    if (metodo == "GET" || metodo == "DELETE") {
+        opciones = {
+            method: metodo,
         };
+    };
 
-        if(metodo == "GET" || metodo == "DELETE"){
+    if (metodo == "POST" || metodo == "PUT") {
+
+        let body = JSON.stringify(datosGuardar);
+
+        if (archivos) {
             opciones = {
                 method: metodo,
+                body: datosGuardar
             };
-        };
-
-        if(metodo == "POST" || metodo == "PUT"){
+        } else {
             opciones = {
                 method: metodo,
                 body: JSON.stringify(datosGuardar),
                 headers: {
                     "Content-Type": "application/json"
                 }
-            }
+            };
         }
-
-        const peticion = await fetch(url, opciones);
+    }
+    const peticion = await fetch(url, opciones);
         const datos = await peticion.json();
 
         cargando = false;
 
-    return {
-        datos,
-        cargando
-    }
+        return {
+            datos,
+            cargando
+        }
 }
