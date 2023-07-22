9840c2898cb753e66971e6d34388065c9a694ec4
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-22 01:18:27 -0500

    Active Eliminar method with a button

--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Global } from '../../helpers/Global';
+import { Peticion } from '../../helpers/Peticion';
 
-export const Listado = ({articulos, setArticulos}) => {
+export const Listado = ({ articulos, setArticulos }) => {
+
+    const eliminar = async (id) => {
+        //alert(id);
+        let { datos } = await Peticion(Global.url + "articulo/" + id, "DELETE");
+        
+        if (datos.status === "success" || datos.status === "Success") {
+            let articulosActualizados = articulos.filter(articulo => articulo._id !== id);
+            setArticulos(articulosActualizados);
+        }
+    }
+
+
     return (
         // {/* Creamos un objeto llamado articulo que contiene los datos recorridos por el array */ }
         articulos.map(articulo => {
@@ -8,14 +22,19 @@ export const Listado = ({articulos, setArticulos}) => {
                 // Creamos una key para react pueda identificar donde poner la información con los datos del array
                 <article key={articulo._id} className="articulo-item">
                     <div className='mascara'>
-                        <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png'></img>
+                        {/* Si la  imagen es diferente a default.png entonces que cargue la imagen del api*/}
+                        {articulo.imagen != "default.png" && <img src={Global.url + "imagen/" + articulo.imagen} />};
+                        {/* Si la  imagen es igual a default.png entonces que cargue la imagen por defecto */}
+                        {articulo.imagen == "default.png" && <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png' />};
                     </div>
                     <div className='datos'>
                         <h3 className="title">{articulo.titulo}</h3>
                         <p className="description">{articulo.contenido}</p>
 
                         <button className="edit">Editar</button>
-                        <button className="delete">Borrar</button>
+                        <button className="delete" onClick={() => {
+                            eliminar(articulo._id)
+                        }}>Borrar</button>
 
                     </div>
                 </article>
