000929236c7959a23a645b4aa1f22a00033f8e2d
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-25 20:49:33 -0500

    Creating routes to load components like Crear, Busqueda, Articulo, Editar and * like Error

--- src/routing/Rutas.jsx
+++ src/routing/Rutas.jsx
@@ -7,6 +7,9 @@ import { Header } from '../components/layout/Header';
 import { Sidebar } from '../components/layout/Sidebar';
 import { Footer } from '../components/layout/Footer';
 import { Crear } from '../components/pages/Crear';
+import { Busqueda } from '../components/pages/Busqueda';
+import { Articulo } from '../components/pages/Articulo'
+import { Editar } from '../components/pages/Editar';
 
 export const Rutas = () => {
   return (
@@ -22,6 +25,15 @@ export const Rutas = () => {
                 <Route path='/inicio' element={<Inicio/>} />
                 <Route path='/articulos' element={<Articulos/>} />
                 <Route path='/crear-articulos' element={<Crear/>} />
+                <Route path='/buscar/:busqueda' element={<Busqueda/>} />
+                <Route path='/articulo/:id' element={<Articulo/>} />
+                <Route path='/editar/:id' element={<Editar/>} />
+
+                <Route path='*' element={
+                  <div className='jumbo'>
+                    <h1>Error 404</h1>
+                  </div>
+                } />
             </Routes>
         </section>
 
