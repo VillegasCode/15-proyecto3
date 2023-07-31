a034f0acb49735fcf2d1fc5e963e97d21395b030
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-30 20:47:36 -0500

    Changing to english language and adding new information

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -4,9 +4,21 @@ import { Link } from "react-router-dom"
 export const Inicio = () => {
   return (
     <div className='jumbo'>
-      <h1>Bienvenido al blog con React</h1>
-      <p>Blog desarrollado con el MERN Stack (Mongo, Express, React y NodeJS)</p>
-      <Link to="/articulos" className='button'>Ver los artículos</Link>
+      <h1>Welcome to Blog App<br/>developed with the next technologies:</h1>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>MongoDB</li>  
+          <li>Express</li>  
+          <li>React</li>
+          <li>NodeJS</li>
+          <li>REST service</li>
+          <li>API</li>
+          <li>HTTP methods</li>
+        </ul>
+        <p>Blog App let us make requests to SERVER and load each one of articles, also let us create articles, read them, update them and delete them, let us make totality CRUD operations of way fast and secure.</p>
+      <Link to="/articulos" className='button-articles'>Show Articles</Link>
     </div>
   )
 }
