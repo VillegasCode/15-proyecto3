a424286d9722167dd9c2e76cc86283cbdbbb08e2
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-30 20:42:05 -0500

    Adding info about how to contact me

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <div className='jumbo'>
+      <h1>
+        Contact
+      </h1>
+      <p>Pop me an email at alexvillegas@empresarioencasa.org or give me a shout on social media</p>
+      <h2>SOCIAL MEDIA</h2>
+      <a className='social-media' href='https://github.com/VillegasCode' target='_blank'><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-256.png" className="social-media-img"/>GitHub</a>
+      <a className='social-media' href='https://www.youtube.com/@AlexanderVillegas' target='_blank'><img src="https://www.designbust.com/download/1005/thumb/transparent_background_youtube_logo_png_thum.png" className='social-media-img' />Youtube</a>
+      <a className='social-media' href='https://www.facebook.com/Alexander.Villegas.Oficial' target='_blank'><img src="https://earthday-365.org/wp-content/uploads/2012/04/facebook-button-transparent.png" className='social-media-img'/>Facebook</a>
+    </div>
+
   )
 }
