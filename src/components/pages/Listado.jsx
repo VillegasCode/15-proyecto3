ca99d56a8c20aff719eee57ce32e592547d10de4
Carlos Villegas <carlos.villegasch@gmail.com>
2023-08-15 20:01:22 -0500

    Button named LEER MÁS added

--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import { Global } from '../../helpers/Global';
 import { Peticion } from '../../helpers/Peticion';
 
+
 export const Listado = ({ articulos, setArticulos }) => {
 
     const eliminar = async (id) => {
         //alert(id);
         let { datos } = await Peticion(Global.url + "articulo/" + id, "DELETE");
-        
+
         if (datos.status === "success" || datos.status === "Success") {
             let articulosActualizados = articulos.filter(articulo => articulo._id !== id);
             setArticulos(articulosActualizados);
@@ -17,9 +18,11 @@ export const Listado = ({ articulos, setArticulos }) => {
 
 
     return (
+
         // {/* Creamos un objeto llamado articulo que contiene los datos recorridos por el array */ }
         articulos.map(articulo => {
             return (
+
                 // Creamos una key para react pueda identificar donde poner la información con los datos del array
                 <article key={articulo._id} className="articulo-item">
                     <div className='mascara'>
@@ -29,8 +32,11 @@ export const Listado = ({ articulos, setArticulos }) => {
                         {articulo.imagen == "default.png" && <img src='https://miro.medium.com/max/1400/1*k0SazfSJ-tPSBbt2WDYIyw.png' />}
                     </div>
                     <div className='datos'>
-                        <h3 className="title"><Link to={"/articulo/"+ articulo._id}>{articulo.titulo}</Link></h3>
+                        <h3 className="title"><Link to={"/articulo/" + articulo._id}>{articulo.titulo}</Link></h3>
                         <p className="description">{articulo.contenido}</p>
+
+                        <button id="readMoreButton" hidden="none">LEER MÁS</button>
+                   
 
                         <Link to={"/editar/" + articulo._id} className='edit'>Edit</Link>
 
@@ -40,7 +46,9 @@ export const Listado = ({ articulos, setArticulos }) => {
 
                     </div>
                 </article>
+
             );
+
         })
     )
 }
