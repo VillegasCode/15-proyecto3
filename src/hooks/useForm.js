4a377923b8d0fdb5c7a196db831f29d0fb80a324
Carlos Villegas <carlos.villegasch@gmail.com>
2023-08-18 19:45:26 -0500

    onchange function named cambiado

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,33 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export const useForm = ( objetoIncial = {} ) => {
+export const useForm = (objetoIncial = {}) => {
 
-const [formulario, setFormulario] = useState(objetoIncial);
+  const [formulario, setFormulario] = useState(objetoIncial);
 
   const serializarFormulario = (formulario) => {
     const formData = new FormData(formulario);
 
     const objetoCompleto = {};
 
-    for(let [name, value] of formData){
+    for (let [name, value] of formData) {
       objetoCompleto[name] = value;
     }
 
     return objetoCompleto;
   }
 
-  const enviado = (e) =>{
-      e.preventDefault();
-      let curso = serializarFormulario(e.target);
+  const enviado = (e) => {
+    e.preventDefault();
+    let curso = serializarFormulario(e.target);
 
-      setFormulario(curso);
+    setFormulario(curso);
 
-      document.querySelector(".codigo").classList.add("enviado");
+    document.querySelector(".codigo").classList.add("enviado");
   }
 
-  const cambiado = ({target}) => {
-    const {name, value} = target;
-    
+  const cambiado = ({ target }) => {
+    const { name, value } = target;   
+
     setFormulario({
       ...formulario,
       [name]: value
@@ -37,6 +37,8 @@ const [formulario, setFormulario] = useState(objetoIncial);
   return {
     formulario,
     enviado,
-    cambiado
+    cambiado,
   }
+
+
 }
